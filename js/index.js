e129d1b0bd6abee595a4a96672891271122cd01e
ourandco <ourandoc@gmail.com>
2020-08-26 19:45:08 +0500

    project progress 40%

--- js/index.js
+++ js/index.js
@@ -26,3 +26,16 @@ INSTRUCTIONS:
 
 // Once you have read the above messages, you can delete all comments. 
 "use strict";
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.backgroundColor = "rgb(104, 187, 203)";
+    document.getElementById("header").style.boxShadow = "0 .6rem 1rem rgba(0,0,0, .2)";
+  } else {
+    document.getElementById("header").style.backgroundColor = "transparent";
+    document.getElementById("header").style.boxShadow = "none";
+  }
+}
